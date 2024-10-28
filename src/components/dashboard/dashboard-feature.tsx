920b436f5f763135b649d2d36aed45b1c4ca072a
Yusef Mosiah Nathanson <yusefnathanson@me.com>
2024-10-28 10:16:54 -0400

    say hi to choir

--- src/components/dashboard/dashboard-feature.tsx
+++ src/components/dashboard/dashboard-feature.tsx
@@ -13,7 +13,7 @@ const links: { label: string; href: string }[] = [
 export default function DashboardFeature() {
   return (
     <div>
-      <AppHero title="gm" subtitle="Say hi to your new Solana dApp." />
+      <AppHero title="gm" subtitle="Say hi to Choir." />
       <div className="max-w-xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
         <div className="space-y-2">
           <p>Here are some helpful links to get you started.</p>
