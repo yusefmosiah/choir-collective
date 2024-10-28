816a6101542e032caf93d985952bf3d3530dd8f3
Yusef Mosiah Nathanson <yusefnathanson@me.com>
2024-10-28 18:01:43 -0400

    conf

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom'
+import 'whatwg-fetch' // Add fetch polyfill
+
+// Mock environment variables
+process.env = {
+  ...process.env,
+  NEXT_PUBLIC_API_URL: 'http://localhost:8000',
+  NODE_ENV: 'test'
+}
--- src/app/__tests__/page.test.tsx
+++ src/app/__tests__/page.test.tsx
@@ -1,11 +1,27 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '../../test-utils'
 import Page from '../page'
+import React from 'react'
+
+// Mock fetch globally
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({
+      NODE_ENV: 'test',
+      NEXT_PUBLIC_API_URL: 'http://localhost:8000'
+    })
+  })
+) as jest.Mock;
 
 describe('Home Page', () => {
-  it('should display "gm" heading', () => {
+  it('should display "gm" heading', async () => {
     render(<Page />)
-    const heading = screen.getByRole('heading', { name: /gm/i })
+    const heading = await screen.findByText('gm')
     expect(heading).toBeInTheDocument()
+  })
+
+  // Clean up after each test
+  afterEach(() => {
+    jest.clearAllMocks()
   })
 })
